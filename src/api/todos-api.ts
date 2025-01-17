80cafccea8619cc40eea760d990c9c1089453336
stopbrother <stopbro0323@naver.com>
2025-01-17 21:10:42 +0900

    refactor: data 구조분해할당

--- src/api/todos-api.ts
+++ src/api/todos-api.ts
@@ -1,12 +1,12 @@
 import { Todo } from "../types/todo.types";
-import { api } from "./axiosInstance";
+import { todoClient } from "./axiosInstance";
 
 export const fetchTodos = async (): Promise<Todo[]> => {
-  const response = await api.get<Todo[]>("/todos");
-  return response.data;
+  const { data } = await todoClient.get<Todo[]>("/todos");
+  return data;
 };
 
-export const fetchTodo = async () => {
-  const response = await api.get("/todos/1");
-  return response.data;
+export const fetchTodo = async (id: Todo["id"]): Promise<Todo> => {
+  const { data } = await todoClient.get<Todo>(`/todos/${id}`);
+  return data;
 };
